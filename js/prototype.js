44bac9f5f4631b98c192b72e06b4fd9afa47bea9
RomellPineda <romell.pineda@gmail.com>
2019-03-20 19:51:06 -0700

    my iframe solution

--- js/prototype.js
+++ js/prototype.js
@@ -1,46 +1,54 @@
 (function ($) {
   "use strict"; // Start of use strict
 
+  $(document).ready(function () {
+    console.log('If you stay ready, you need not get ready');
+    // var width = $(window).width();
+    // console.log('jQuery says width = ', width);
+
+    // Scroll Directives
+    $(window).scroll(function (event) {
+      var target = $('#shift');
+      var distance = $(window).scrollTop();
+      // console.log('distance: ', distance);
+
+      // Slide Right
+      if (distance < 800) {
+        target.css('left', distance);
+      }
+
+      // Fade Out
+      if (distance > 1100) {
+        $('#text_area').fadeOut(2000);
+      }
+
+      // Fade In
+      if (distance < 1080) {
+        $('#text_area').fadeIn(2000);
+      }
+
+    });
+
+    $('.dropdown-trigger').dropdown({
+      hover: true,
+      constrainWidth: false
+    });
+
+    $('.modal').modal({
+      dismissible: false
+    });
+
+    $('.modal-close').click(function () {
+      $('.video').each(function () {
+        this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+      });
+    });
+
+    $('#submit').hover(function () {
+      $(this).css('background-color', 'rgb(132, 133, 135)');
+    }, function () {
+      $(this).css('background-color', 'rgb(104, 103, 103)');
+    });
+  });
+
 })(jQuery); // End of use strict
-
-$(document).ready(function () {
-  console.log('If you stay ready, you need not get ready');
-  // var width = $(window).width();
-  // console.log('jQuery says width = ', width);
-
-  // Scroll Directives
-  $(window).scroll(function (event) {
-    var target = $('#shift');
-    var distance = $(window).scrollTop();
-    // console.log('distance: ', distance);
-
-    // Slide Right
-    if (distance < 800) {
-      target.css('left', distance);
-    }
-
-    // Fade Out
-    if (distance > 1100) {
-      $('#text_area').fadeOut(2000);
-    }
-
-    // Fade In
-    if (distance < 1080) {
-      $('#text_area').fadeIn(2000);
-    }
-
-  })
-
-  $('.dropdown-trigger').dropdown({
-    hover: true,
-    constrainWidth: false
-  });
-
-  $('.modal').modal();
-
-  $('#submit').hover(function () {
-    $(this).css('background-color', 'rgb(132, 133, 135)');
-  }, function () {
-    $(this).css('background-color', 'rgb(104, 103, 103)');
-  });
-});
